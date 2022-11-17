1268d93042fd82c5f5195f27c5dcf53eade9925b
Svitlana R <sv73@ukr.net>
2022-11-17 18:41:11 +0200

    Created 2 new files

---
+++ script.js
@@ -0,0 +1,10 @@
+`use strict`;
+
+// #1 Найкращі друзі
+
+let myFriends = [Гоша, Настя, Ваня, Світлана, Катерина, Наташа];
+
+for (i = 0; i < myFriends.length; i++) { 
+    myFriends[i] = myFriends[i] + <p></p>
+    
+}
