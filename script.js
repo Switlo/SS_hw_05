cb4c52374adcb87cdec148cae6111fe80a315a22
Svitlana R <sv73@ukr.net>
2022-11-17 20:37:40 +0200

    Added the solution of 1st task

--- script.js
+++ script.js
@@ -2,9 +2,14 @@
 
 // #1 Найкращі друзі
 
-let myFriends = [Гоша, Настя, Ваня, Світлана, Катерина, Наташа];
+let myFriends = ["Гоша", "Настя", "Ваня", "Світлана", "Катерина", "Наташа"];
 
 for (i = 0; i < myFriends.length; i++) { 
-    myFriends[i] = myFriends[i] + <p></p>
-    
+    $("body").append(myFriends[i] + "<p></p>");
 }
+
+$("#first-heading").text("Мої найкращі друзі"); 
+$("p").append("кращий/-а")
+$("p").fadeOut(2000).fadeIn(2000).fadeOut(2000).fadeIn(1000);
+
+// ??? Используйте метод hide и метод fadeIn, чтобы имена плавно возникали на экране.
